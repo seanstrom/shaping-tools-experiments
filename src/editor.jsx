d544908335da50dfdb7089543ab61303f1b43385
Sean Hagstrom <sean@seanhagstrom.com>
2024-11-25 12:20:49 -0800

    chore: remove unused import

--- src/editor.jsx
+++ src/editor.jsx
@@ -1,5 +1,5 @@
 import {$getRoot, $getSelection} from 'lexical';
-import React, {useEffect} from 'react';
+import React from 'react';
 
 import {AutoFocusPlugin} from '@lexical/react/LexicalAutoFocusPlugin';
 import {LexicalComposer} from '@lexical/react/LexicalComposer';
